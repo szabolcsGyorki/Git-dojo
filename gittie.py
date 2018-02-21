923ef7050290fec6d8bfea412221ff394235c2f8
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-02-21 09:32:46 +0100

    Modified introduce function

--- gittie.py
+++ gittie.py
@@ -1,6 +1,9 @@
 def introduce():
     print("Hello, I'm Gittie!")
-    
+    print("I'm a very cheerful, little gittie-cub!")
+    print("I can tell you jokes and I can shout at you if you are being silly! :D")
+
+
 def joke():
     print("hahahaha")    
 
